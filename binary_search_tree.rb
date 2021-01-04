092c37ea387a0023b6d6b203b0deef0154fe0ef4
Nick McDaniel <nickamcd@gmail.com>
2021-01-04 10:55:40 -0800

    Insert function added. Does not yet work for values greater than the current largest value in the tree.

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,15 +1,10 @@
 class Node
-  include Comparable
   attr_accessor :data, :left, :right
 
-  def initialize(data = il, left = nil, right = nil)
+  def initialize(data = nil)
     @data = data
-    @left = left
-    @right = right
-  end
-
-  def <=>(other)
-    @data <=> other.data
+    @left = nil
+    @right = nil
   end
 end
 
@@ -21,6 +16,7 @@ class Tree
   end
 
   def build_tree(data=[], start_pointer, end_pointer)
+    # Base case.
     return nil if end_pointer < start_pointer
 
     mid = (start_pointer + end_pointer) / 2
@@ -32,8 +28,26 @@ class Tree
     node
   end
 
-  def insert(value)
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
 
+  def insert(value, node = root)
+    if node.data.nil?
+      new_node = Node.new(value)
+      return new_node
+    end
+    p value
+    p node.data
+
+
+    if value < node.data
+      node.left.nil? ? node.left = Node.new(value) : insert(value, node.left)
+    else
+      node.right.nil? ? node.right = Node.new(value) : insert(value, node.right)
+    end
   end
 
   def delete(value)
@@ -43,4 +57,12 @@ end
 
 test_tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 
-p test_tree
+puts test_tree.pretty_print
+
+test_tree.insert(42)
+test_tree.insert(9999)
+test_tree.insert(6767)
+test_tree.insert(19)
+
+
+puts test_tree.pretty_print
