4a16b89e4eab6a596d86bed02789f971634435a9
Nick McDaniel <nickamcd@gmail.com>
2021-01-11 15:16:53 -0800

    Added find function.

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -76,15 +76,27 @@ class Tree
     node = node.left until node.left.nil?
     node
   end
+
+  def find(value, node = @root)
+    # Base case. Root is the desired valued
+    return node if value == node.data
+
+    if value < node.data
+      find(value, node.left)
+    elsif value > node.data
+      find(value, node.right)
+    else
+      node
+    end
+  end
 end
 
 test_tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 
 puts test_tree.pretty_print
 
-puts
-puts "Inserting 42, 15, 3, and 8000."
-puts
+puts "Inserting 42, 15, 3, then 8000.\n\n"
+
 
 test_tree.insert(42)
 test_tree.insert(15)
@@ -93,12 +105,17 @@ test_tree.insert(8000)
 
 puts test_tree.pretty_print
 
-puts
-puts "Deleting 67, 23, and 5."
-puts
+puts "Deleting 67, 23, 5, then 3.\n\n"
 
 test_tree.delete(67)
 test_tree.delete(23)
 test_tree.delete(5)
+test_tree.delete(3)
 
 puts test_tree.pretty_print
+
+puts "Testing find on 42, 8, and 15\n\n"
+
+puts test_tree.find(42).data
+puts test_tree.find(8).data
+puts test_tree.find(15).data
